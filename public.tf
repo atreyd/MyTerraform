9a238ca6928fecdb7258c0dcf998dac2bce25445
Deepak Atrey <datrey@pdxinc.com>
2018-08-16 09:22:00 +0530

    New files and changes

--- public.tf
+++ public.tf
@@ -30,18 +30,6 @@ resource "aws_security_group" "VB" {
         Name = "VBServerSG"
     }
 }
-
-data "template_cloudinit_config" "master" {
-  gzip          = true
-  base64_encode = true
-
-  # get master user_data
-  part {
-    filename     = "master.cfg"
-    content_type = "text/part-handler"
-    content      = "${data.template_file.userdata_master.rendered}"
-  }
-}
 resource "aws_instance" "VB-1" {
     ami = "${lookup(var.amis, var.aws_region)}"
     availability_zone = "ap-south-1a"
@@ -51,8 +39,8 @@ resource "aws_instance" "VB-1" {
     subnet_id = "${aws_subnet.ap-south-1a-public.id}"
     associate_public_ip_address = true
     source_dest_check = false
-    user_data_base64 = "${data.template_cloudinit_config.master.rendered}"
-    
+    user_data = "${data.template_cloudinit_config.config.rendered}"
+
     tags {
         Name = "VB Server 1"
     }
