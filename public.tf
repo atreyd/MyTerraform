8e0801271d1392919335e04c3b808c4ef5409171
Deepak Atrey <datrey@pdxinc.com>
2018-08-28 13:02:00 +0530

    Updated private, public and variable

--- public.tf
+++ public.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "VB" {
     }
 }
 resource "aws_instance" "Local_VM_Pub" {
-    ami = "${lookup(var.AMIS.default, var.aws_region)}"
+    ami = "${lookup(var.AMIS["pub_ami"], var.aws_region)}"
     availability_zone = "${lookup(var.AZones[1])}"
     instance_type = "t2.micro"
     key_name = "${var.aws_key_name}"
